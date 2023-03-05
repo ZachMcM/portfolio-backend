a3b64ef7f1ad280c759843a83a8072898061a3a0
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-05 01:26:14 -0800

    cors

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const octokit = new Octokit({
 const express = require('express')
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.get('/', async (req, res) => {
     const response = await octokit.request('GET /users/{username}/repos', {
         username: 'zachmcm',
@@ -27,12 +34,6 @@ app.get('/', async (req, res) => {
         }
     })
     res.json(repoList)
-})
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
 })
 
 app.listen(port, () => {
