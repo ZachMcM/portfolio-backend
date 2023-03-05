fb08c6f9fcec0d82bd6b290da3b3dc3c795dd6d7
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-05 00:23:29 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const { Octokit } = require('octokit')
+const octokit = new Octokit({
+    auth: process.env.GITHUB_AUTH_TOKEN
+})
+
+const express = require('express')
+const app = express()
+
+app.get('/', async (req, res) => {
+    const response = await octokit.request('GET /users/{username}/repos', {
+        username: 'zachmcm',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+        sort: 'updated',
+    })
+    const repoList = response.data.map(repo => {
+        return {
+            homepage: repo.homepage,
+            name: repo.name,
+            url: repo.html_url,
+            description: repo.description,
+            language: repo.language,
+        }
+    })
+    res.json(repoList)
+})
+
+
+app.listen(8000, () => {
+    console.log('Listening on port 8000')
+})
+
