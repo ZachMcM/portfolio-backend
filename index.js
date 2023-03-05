3331d9b13a8486e8d6af1b3a701c26f173df9a1a
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-05 02:38:08 -0800

    adding stats

--- index.js
+++ index.js
@@ -9,14 +9,7 @@ const octokit = new Octokit({
 const express = require('express')
 const app = express()
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-
-app.get('/', async (req, res) => {
+async function getRepos() {
     const response = await octokit.request('GET /users/{username}/repos', {
         username: 'zachmcm',
         headers: {
@@ -33,7 +26,38 @@ app.get('/', async (req, res) => {
             language: repo.language,
         }
     })
-    res.json(repoList)
+    return repoList
+}
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+app.get('/repos', async (req, res) => {
+    const repos = await getRepos()
+    res.json(repos)
+})
+
+app.get('/stats', async (req, res) => {
+    const repos = await getRepos()
+    const langauges = []
+    repos.forEach(repo => {
+        if (repo.language) {
+            const language = langauges.find(language => language.name === repo.language)
+            if (language) {
+                language.count++
+            } else {
+                langauges.push({name: repo.language, count: 1})
+            }
+        }
+    })
+    res.json({
+        langauges: langauges,
+        total: repos.length
+    })
 })
 
 app.listen(port, () => {
